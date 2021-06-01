125d96eeb609f8b3f2ae01e2c79b216f49a01bcf
Nafisul khondaker <nafisulkhondaker@email.arizona.edu>
2021-06-02 01:58:31 +0600

    test

--- startTracking.js
+++ startTracking.js
@@ -8,7 +8,7 @@ AFRAME.registerComponent('start-tracking', {
       mod.setAttribute('position', '0 100 1000')
       mod.setAttribute('rotation', '0 90 0')
       mod.setAttribute('visible', 'false')
-      mod.setAttribute('scale', '0.0001 0.0001 0.0001')
+      mod.setAttribute('scale', '100 100 100')
 
       mod.setAttribute('gltf-model', '#3dmodel')
       this.el.sceneEl.appendChild(mod)
@@ -21,7 +21,10 @@ AFRAME.registerComponent('start-tracking', {
         //   easing: 'easeOutElastic',
         //   dur: 800,
         // })
-        mod.setAttribute('animation-mixer', {})
+        mod.setAttribute('animation-mixer', {
+            // clip: 'Animation',
+            // loop: 'repeat'
+        })
       })
       text.setAttribute('visible', 'false')
     }, 15000)
