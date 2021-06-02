e13ddb08ca5e0da8243316bbaccdb02336702f5c
Nafisul khondaker <nafisulkhondaker@email.arizona.edu>
2021-06-02 22:22:54 +0600

    test

--- startTracking.js
+++ startTracking.js
@@ -5,7 +5,7 @@ AFRAME.registerComponent('start-tracking', {
 
     setTimeout(() => {
 
-      mod.setAttribute('position', '0 -100 1000')
+      mod.setAttribute('position', '0 100 -1000')
       mod.setAttribute('rotation', '0 90 0')
       mod.setAttribute('visible', 'false')
       mod.setAttribute('scale', '100 100 100')
