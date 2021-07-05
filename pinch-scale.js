e74712e8488f58db3544c74670aa9e1729222b16
Nafisul khondaker <nafisulkhondaker@email.arizona.edu>
2021-07-05 06:26:34 +0600

    test

---
+++ pinch-scale.js
@@ -0,0 +1,32 @@
+
+AFRAME.registerComponent('pinch-scale', {
+    schema: {
+      min: {default: 0.1},
+      max: {default: 50}
+    },
+    init: function() {
+      const container = document.getElementById('container')
+      this.initialScale = this.el.object3D.scale.clone()
+      this.scaleFactor = 1
+      this.handleEvent = this.handleEvent.bind(this)
+      this.el.sceneEl.addEventListener('twofingermove', this.handleEvent)
+    },
+    update: function () {
+        this.initialScale = this.el.object3D.scale.clone()
+        this.scaleFactor = 1
+        this.handleEvent = this.handleEvent.bind(this)
+        this.el.sceneEl.addEventListener('twofingermove', this.handleEvent)
+        container.innerHTML = "Scale: " + this.el.object3D.scale.x
+    },
+    remove: function() {
+      this.el.sceneEl.removeEventListener('twofingermove', this.handleEvent)
+    },
+    handleEvent: function(event) {
+      this.scaleFactor *= 1 + event.detail.spreadChange / event.detail.startSpread
+      this.scaleFactor = Math.min(Math.max(this.scaleFactor, this.data.min), this.data.max)
+  
+      this.el.object3D.scale.x = this.scaleFactor * this.initialScale.x
+      this.el.object3D.scale.y = this.scaleFactor * this.initialScale.y
+      this.el.object3D.scale.z = this.scaleFactor * this.initialScale.z
+    }
+  })
